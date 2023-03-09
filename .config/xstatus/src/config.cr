f4b4751d1c03e1ca129381db423cbe8839ddab0e
Pragu3 <6oxt1xhr@duck.com>
2023-03-09 15:29:10 -0500

    update

---
+++ .config/xstatus/src/config.cr
@@ -0,0 +1,33 @@
+require "io"
+
+module Config
+    extend self
+    def run(args : Array(String)) : String
+        io = IO::Memory.new
+        Process.run(args.join(" "), shell: true, output: io)
+        io.to_s
+    end
+    
+    def text(s : String, a : Array(String)) : String
+        s
+    end
+    
+    def date(s : String, a : Array(String)) : String
+        outp = run(["date", a.join(" ")])
+        s.gsub("%s", outp).rstrip
+    end
+
+    struct Field
+        getter function : String, Array(String) -> String
+        getter format : String
+        getter arguments : Array(String)
+
+        def initialize(@function, @format, @arguments); end
+    end
+
+    CONFIG = [
+#                           FUNCTION                                 FORMAT                       ARGUMENTS        
+        Field.new(->text(String, Array(String)),                     "Pr4gu3",                      [""]),
+        Field.new(->date(String, Array(String)),                     "%s",                          ["+%r"]),
+    ]
+end
